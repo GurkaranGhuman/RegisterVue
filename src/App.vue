4272d567c1f65f1d5d6d90e7fb5402dba082bc2f
gurkaran <gurkaran361@gmail.com>
2025-05-07 16:38:51 +0530

    slots-and-props

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <the-header title="Remember Me"></the-header>
+  <stored-resource :resources="storedResoucevalues"></stored-resource>
+</template>
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import StoredResource from './components/LearningResources/StoredResource.vue';
+export default {
+  components: {
+    TheHeader,
+    StoredResource,
+  },
+  data() {
+    return {
+      storedResoucevalues: [
+        {
+          id: 'official guide',
+          title: 'official guide',
+          description: 'The official vue.js documentation',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'Google',
+          title: 'Google',
+          description: 'The official google documentation',
+          link: 'https://google.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
